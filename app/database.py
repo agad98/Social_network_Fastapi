a4dad0f780af19c59225392fdc3203d74d3aaa09
Gad <67938589+agad98@users.noreply.github.com>
2022-10-06 10:43:42 -0500

    updating env vars case trial 4: works now

--- app/database.py
+++ app/database.py
@@ -1,15 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
+#import psycopg2
+#from psycopg2.extras import RealDictCursor
+#import time
 from .config import settings
-import os
+#import os
 
 #SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-#SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
-SQLALCHEMY_DATABASE_URL = 'postgresql://znrdfgxqfwomik:66002f04b8bc766bc2027a1b3725ff7c68f7a8c217deecc1e642881c2c24684c@ec2-54-86-106-48.compute-1.amazonaws.com:5432/d6e2ep19hfad6t'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
