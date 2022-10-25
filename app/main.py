598aa1fc80e656da62cbeb16a06f11e1ff20a596
Gad <67938589+agad98@users.noreply.github.com>
2022-10-25 10:57:01 -0500

    adding ubuntu deploy

--- app/main.py
+++ app/main.py
@@ -26,4 +26,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World: Deployed from CI/CD pipeline"}
+    return {"message": "Hello World: pushed out to Ubuntu"}
